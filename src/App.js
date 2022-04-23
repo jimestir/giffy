066450e5d1ae1f60d3222621f75852a9326384e2
jimmyvergara03 <jimmyestir@gmail.com>
2022-04-23 10:42:46 -0400

    Add getGifs.jsx

--- src/App.js
+++ src/App.js
@@ -1,30 +1,22 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-
-const API_URL =
-  "https://api.giphy.com/v1/gifs/search?api_key=6CUKWhv8ZLkdXLj17yTh7n340Z3tTxEf&q=panda&limit=10&offset=0&rating=g&lang=en";
+import getGifs from "./getGifs";
 
 function App() {
   const [gifs, setGifs] = useState([]);
 
   useEffect(() => {
-    fetch(API_URL)
-      .then((res) => res.json())
-      .then((res) => {
-        const { data = [] } = res;
-        if (Array.isArray(data)) {
-          const gifs = data.map((image) => image.images.downsized_medium.url);
-          setGifs(gifs);
-        } else alert("Error not can't find gifs");
-      })
-      .catch((error) => console.error(error));
+    getGifs({ keyword: "panda" }).then((gifs) => setGifs(gifs));
   }, []);
-
   return (
     <div className="App">
       <section className="App-content">
         {gifs.map((singleGif) => (
-          <img src={singleGif} />
+          <div>
+            <h4>{singleGif.title}</h4>
+            <small>{singleGif.id}</small>
+            <img alt={singleGif.title} src={singleGif.url} />
+          </div>
         ))}
         <button onClick={() => setGifs()}>Change</button>
       </section>
