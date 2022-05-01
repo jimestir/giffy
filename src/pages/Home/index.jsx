74edee607ac09ec07518f433c6d49f6bd50d50a6
jimestir <jimmyestir@gmail.com>
2022-05-01 18:46:05 -0400

    Add last Search

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useLocation } from "wouter";
+import ListOfGifs from "../../components/ListOfGifs";
+import getGifs from "../../services/getGifs";
 import { Ul, LinkStyled } from "../../components/styledComponents";
 
 const POUPLUAR_GIFS = ["pocoyo", "Venezuela", "rick", "dog", "cat"];
@@ -7,8 +9,16 @@ const POUPLUAR_GIFS = ["pocoyo", "Venezuela", "rick", "dog", "cat"];
 function Home() {
   const [keyword, setKeyword] = useState("");
   const [path, pushLocation] = useLocation();
+  const [gifs, setGifs] = useState([]);
+  const [loading, setLoanding] = useState(false);
 
-  console.log(path);
+  useEffect(() => {
+    setLoanding(true);
+    getGifs({ keyword: "rick" }).then((gifs) => {
+      setGifs(gifs);
+      setLoanding(false);
+    });
+  }, [keyword]);
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -30,6 +40,10 @@ function Home() {
         />
         <button type="submit">search</button>
       </form>
+
+      <h3>Ultima busqueda ...</h3>
+      <ListOfGifs gifs={gifs} />
+
       <h3>Los gifs más populares</h3>
       <Ul>
         {POUPLUAR_GIFS.map((popularGifs) => {
