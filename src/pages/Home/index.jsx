4f5405e89d603fd6bdc5100087baea8247cfd472
jimestir <jimmyestir@gmail.com>
2022-05-01 14:44:12 -0400

    Add search input and submit button

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import { useLocation } from "wouter";
 import { Ul, LinkStyled } from "../../components/styledComponents";
 
 const POUPLUAR_GIFS = ["pocoyo", "Venezuela", "rick", "dog", "cat"];
 
-function Home(props) {
+function Home() {
+  const [keyword, setKeyword] = useState("");
+  const [path, pushLocation] = useLocation();
+
+  console.log(path);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    //navegar a otra router
+    pushLocation(`/search/${keyword}`);
+  };
+  const handleChange = (event) => {
+    setKeyword(event.target.value);
+  };
+
   return (
     <>
+      <form onSubmit={handleSubmit}>
+        <input
+          placeholder="Search a gif here..."
+          onChange={handleChange}
+          type="text"
+          value={keyword}
+        />
+        <button type="submit">search</button>
+      </form>
       <h3>Los gifs más populares</h3>
       <Ul>
         {POUPLUAR_GIFS.map((popularGifs) => {
