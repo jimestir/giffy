61ec465184994029fe951469b4a5a38dca5f6b3a
jimestir <jimmyestir@gmail.com>
2022-05-07 14:32:00 -0400

    Change in the structe

--- src/services/getGifs.jsx
+++ src/services/getGifs.jsx
@@ -1,21 +1,23 @@
 const apiKey = "6CUKWhv8ZLkdXLj17yTh7n340Z3tTxEf&q";
 
+const formApiResponseToGifs = (apiResponse) => {
+  const { data = [] } = apiResponse;
+  if (Array.isArray(data)) {
+    const gifs = data.map((image) => {
+      const { images, title, id } = image;
+      const { url } = images.downsized_medium;
+      return { title, id, url };
+    });
+    return gifs;
+  } else alert("Error not can't find gifs");
+};
+
 function getGifs({ keyword = "rick and morty" } = {}) {
   const API_URL = ` https://api.giphy.com/v1/gifs/search?api_key=${apiKey}=${keyword}&limit=10&offset=0&rating=g&lang=en`;
 
   return fetch(API_URL)
     .then((res) => res.json())
-    .then((res) => {
-      const { data = [] } = res;
-      if (Array.isArray(data)) {
-        const gifs = data.map((image) => {
-          const { images, title, id } = image;
-          const { url } = images.downsized_medium;
-          return { title, id, url };
-        });
-        return gifs;
-      } else alert("Error not can't find gifs");
-    })
+    .then(formApiResponseToGifs)
     .catch((error) => console.log(error));
 }
 
